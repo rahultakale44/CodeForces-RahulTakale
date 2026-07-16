afbf5b87074fa9e9159c143cf040f223e81cf29a
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-17 00:05:56 +0530

    71A

---
+++ 71A – Way Too Long Words/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String word = sc.next();
+
+            if (word.length() > 10) {
+                char first = word.charAt(0);
+                char last = word.charAt(word.length() - 1);
+                int middleLetters = word.length() - 2;
+
+                System.out.println(first + "" + middleLetters + last);
+            } else {
+                System.out.println(word);
+            }
+        }
+
+        sc.close();
+    }
+}
