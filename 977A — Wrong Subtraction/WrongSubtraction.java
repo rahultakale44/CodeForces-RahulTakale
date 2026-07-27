7092e18295aa0998d7976091dcf950c196d54ccb
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-27 23:01:52 +0530

    Wrong Subtraction Problem

---
+++ 977A — Wrong Subtraction/WrongSubtraction.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WrongSubtraction {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        for (int i = 0; i < k; i++) {
+
+            if (n % 10 == 0) {
+                n = n / 10;
+            } else {
+                n = n - 1;
+            }
+        }
+
+        System.out.println(n);
+
+        scanner.close();
+    }
+}
